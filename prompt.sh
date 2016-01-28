2574d97f2a995e03c4fa480df81a4a2478e0bbe6
Robert Egan <robert.egan@zefr.com>
2016-01-28 12:27:47 -0800

    Removed unused function

--- prompt.sh
+++ prompt.sh
@@ -1,16 +1,3 @@
-#find_git_branch() {
-#  # Based on: http://stackoverflow.com/a/13003854/170413
-#  local branch
-#  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-#    if [[ "$branch" == "HEAD" ]]; then
-#      branch='detached*'
-#    fi
-#    git_branch="($branch)"
-#  else
-#    git_branch=""
-#  fi
-#}
-
 find_git_branch() {
   local branch
   if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
@@ -24,11 +11,9 @@ find_git_branch() {
 
   local status=$(git status --porcelain 2> /dev/null)
   if [[ "$status" != "" ]]; then
-#    git_dirty='*'
     git_clean=''
     git_dirty="$git_branch"
   else
-#    git_dirty=''
     git_clean="$git_branch"
     git_dirty=''
   fi
